214db74cc4ca9468aed02deb2b29c13248390aa9
krillan <pushkinidze49@gmail.com>
2024-03-04 18:30:28 +0300

    items controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,25 @@
+class ItemsController < ApplicationController
+  before_action :set_item!
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @item.upate item_params
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_item!
+    @item = Item.find params[:id]
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :lock_version)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'items/show'
+  get 'items/edit'
+  get 'items/update'
+  get 'articles/show'
+  get 'articles/edit'
+  get 'articles/update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
